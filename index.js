a4c3cd9ed142d6daf07aec8db6487c2ebfd2d992
AnikHaque <ekramul15-811@diu.edu.bd>
2024-12-02 01:58:05 +0600

    mongodb setup done

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ const port = process.env.PORT || 5000
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 app.use(express.json())
 app.use(cors())
+
+// mongodb setup///
+const uri = `mongodb+srv://${process.env.MEDIMART_USER}:${process.env.MEDIMART_PASS}@cluster0.phy8j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
