000c5e05d3f683ab0c79e33f028bbe93600c1beb
AnikHaque <ekramul15-811@diu.edu.bd>
2025-09-05 00:37:26 +0600

    category delete  api  done

--- index.js
+++ index.js
@@ -172,6 +172,14 @@ async function run() {
       res.send(result)
     })
 
+    //  category delete/
+    app.delete("/category/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) };
+      const result = await categoryCollection.deleteOne(query);
+      res.send(result)
+    })
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
   }
